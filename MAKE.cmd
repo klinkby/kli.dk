ab644e04ed95ae3f70184a3513a7bbc4902b01a3
Mads Breusch Klinkby <1681112+klinkby@users.noreply.github.com>
2021-04-10 14:05:13 +0200

    remove obsolete credentials

--- MAKE.cmd
+++ MAKE.cmd
@@ -1,5 +1,5 @@
-@SET AZURE_STORAGE_ACCOUNT=klidkstatic
-@SET AZURE_STORAGE_KEY=YN4E3Eh6YersWTLl1RHmbaD4qIyWn6mq1L6lrNMeVXnKhsIpgUyxvNxBO4UfjYeJKNj2yvO/3NfRYsKG4ZMwHA==
+@SET AZURE_STORAGE_ACCOUNT=??
+@SET AZURE_STORAGE_KEY=??
 pushd src
 ..\hugo
 ..\hugo deploy --maxDeletes -1
